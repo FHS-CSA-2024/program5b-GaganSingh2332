ff4df53fae1963d194d23fa1a8bf7c2c7b971738
GaganSingh2332 <gagans0422@gmail.com>
2024-09-11 19:50:44 -0700

    Update CarTester.java
    almost done with cartester

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+public class CarTester {
+    public Car addCar() {
+    Scanner elCaro = new Scanner(System.in);
+    System.out.println("Enter Car Name");
+    String carName = elCaro.nextLine();
+    System.out.println("Enter Miles Driven");
+    double mileDriven = elCaro.nextDouble();
+    System.out.println("Enter Gallons Used");
+    double gallonsUsed = elCaro.nextDouble();
+    Car Car1 = new Car(carName, mileDriven, gallonsUsed);
+    return Car1;
+    }
+    
+    public static void main(String[] args) {
+    
 
+    
+    
+        
+        
+       
+     
+    }
+}
